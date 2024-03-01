07894911f13b846ae0da8d7721de9cf10ce2294d
arammartirosyan1 <arammartirosyan788@gmail.com>
2024-03-01 14:18:33 +0400

    print

--- test.py
+++ test.py
@@ -4,7 +4,25 @@ import pandas as pd
 def clean_address(address, region_strings):
     for region in region_strings:
         address = address.replace(region, "").strip(',').strip().strip(',').strip()
-    return address
+    if 'Վայոց Ձոր' in address.title():
+        region = 'Վայոց Ձոր'
+        data = address.replace(region, "") .strip(',').strip().strip(',').strip()
+        return region, '', data
+    else:
+        double_cities = {
+            "Նոր Նորք": "Նոր Նորք",
+            "Զեյթուն": "Քանաքեռ-Զեյթուն",
+            "Մալաթիա": "Մալաթիա-Սեբաստիա",
+            "Մարաշ": "Նորք-Մարաշ"
+        }
+        for city, city_alias in double_cities.items():
+            if city in str(address).title():
+                region = "Երևան"
+                city = city_alias
+                data = address.replace(city, '').strip(",").strip().strip(",").strip()
+                data = data.replace(region, '').strip(",").strip().strip(",").strip()
+                return region, city, data
+    return '', '', address
 
 
 def strip_address(address):
@@ -27,7 +45,6 @@ def find_region_city(region, data):
             city = row.iloc[0]
             data = data.replace(city, '').strip(',').strip().strip(",").strip()
             return city, data
-    
     return '', data
 
 
@@ -37,51 +54,22 @@ def check_special_case(region):
     return region
 
 
-def find_city(address, new_data):
+def find_city(data, new_data):
     df = pd.read_excel('Book1.xlsx')
     for reg_ct in new_data[:2]:
         mask = df.iloc[:, 0] == reg_ct
-        print(reg_ct)
         matching_rows = df[mask]
         if not matching_rows.empty:
             city = matching_rows.iloc[0, 0]
             region = matching_rows.iloc[0, 1]
-            data = str(address).replace(reg_ct, '').strip(",").strip()
+            data = str(data).replace(reg_ct, '').strip(",").strip()
             data = data.replace(region, '').strip(",").strip()
             return region, city, data
-    return '', '', address
+    return '', '', data
 
 
 def check_default_case(address):
     return 'Երևան', 'Երևան', address
-
-
-def find_region_in_double(address):
-    double_cities = {
-        "Նոր Նորք": "Նոր Նորք",
-        "Զեյթուն": "Քանաքեռ-Զեյթուն",
-        "Մալաթիա": "Մալաթիա-Սեբաստիա",
-        "Մարաշ": "Նորք-Մարաշ"
-    }
-    for city, city_alias in double_cities.items():
-        if city in address:
-            region = "Երևան"
-            city = city_alias
-            data = address.replace(city, '').strip(",").strip().strip(",").strip()
-            return region, city, data
-    return '', '', address
-
-
-def vayoc_dzor(region, city, address):
-    if region == 'Վայոց':
-        region = 'Վայոց Ձոր'
-        data = address.replace(region, "").strip(",").strip().strip(",").strip()
-        if city == 'Վայոց':
-            city = 'Վայոց Ձոր'
-        else:
-            data = data.replace(city, "").strip(",").strip().strip(",").strip()
-        return region, city, data
-    return '', '', ''
 
 
 def bank_region(address):
@@ -90,45 +78,42 @@ def bank_region(address):
     region_strings = ["ք․", "ք.", "Ք․", "Ք.", "քաղաք", "Քաղաք", "մ․", "մ.", "Մ․", "Մ.", "մարզ", "Մարզ", "ՀՀ", "հհ",
                       "գ․", "գ.", "Գ․", "Գ.", "գյուղ", "Գյուղ", "հ․", "հ.", "Հ․", "Հ.", "համայնք", "Համայնք"]
 
-    address = clean_address(address, region_strings)
-    new_data = strip_address(address)
-
-    region, data = find_region(address, new_data, regions)
-    if region:
-        print(region)
-        city, data = find_region_city(region, data)
-        if city:
-            print(city)
-            print(data)
-        else:
-            city = check_special_case(region)
-            print(city)
-            print(data)
+    region, city, data = clean_address(address, region_strings)
+    if region and city:
+        return region, city, data
+    elif region:
+        if not city:
+            city, data = find_region_city(region, data)
+            if city:
+                return region, city, data
+            else:
+                city = check_special_case(region)
+                return region, city, data
     else:
-        region, city, data = find_city(address, new_data)
+        new_data = strip_address(data)
+        region, data = find_region(data, new_data, regions)
         if region:
-            print(region)
-            print(city)
-            print(data)
+            city, data = find_region_city(region, data)
+            if city:
+                return region, city, data
+            else:
+                city = check_special_case(region)
+                return region, city, data
         else:
-            region, city, data = check_default_case(address)
-            print(region)
-            print(city)
-            print(data)
-
-    region, city, data = find_region_in_double(address)
-    if region:
-        print(region)
-        print(city)
-        print(data)
-
-    region, city, data = vayoc_dzor(region, city, address)
-    if region:
-        print(region)
-        print(city)
-        print(data)
+            region, city, data = find_city(data, new_data)
+            if region:
+                return region, city, data
+            else:
+                region, city, data = check_default_case(data)
+                return region, city, data
 
 
-bank_region("ՀՀ Երևան ավան մ․ ք․ ,Քոչար գ․ փողոց, 51/2/հիսունմեկ կոտորակ երկու/շենք, 13/1/տասներեք կոտորակ մեկ/բնակարան")
+def print_bank_region(address):
+    region, city, data = bank_region(address)
+    print("Region:", region)
+    print("City:", city)
+    print("Data:", data)
 
+
+print_bank_region("ՀՀ Արաբկիր մ․ ք․ ,Քոչար գ․ փողոց, 51/2/հիսունմեկ կոտորակ երկու/շենք, 13/1/տասներեք կոտորակ մեկ/բնակարան")
 
